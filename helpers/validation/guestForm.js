929ed66fa510302eea0e8765d01181317e1a7c7b
Abubakr Fazilov <abubakrfazilov98@gmail.com>
2021-05-29 20:07:55 +0300

    fix: added checking for whitespace

--- helpers/validation/guestForm.js
+++ helpers/validation/guestForm.js
@@ -4,9 +4,9 @@ module.exports = (body) => {
   let regex = /^[a-zA-Z0-9 _]*$/
   let errors = []
 
-  if (!name) errors.push('field "name" must be provided and cannot be empty')
-  if (!message) errors.push('field "message" must be provided and cannot be empty')
-  if (!date) errors.push('field "date" must be provided and cannot be empty')
+  if (!name?.trim()) errors.push('field "name" must be provided and cannot be empty')
+  if (!message?.trim()) errors.push('field "message" must be provided and cannot be empty')
+  if (!date?.trim()) errors.push('field "date" must be provided and cannot be empty')
 
   if (!name.match(regex))
     errors.push('use only letters, numbers and underscores for "name" field!')
